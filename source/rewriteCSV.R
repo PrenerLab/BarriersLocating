05d38ece743a05b978099d8a37106ac3a4ffd325
Christopher Prener <chris.prener@slu.edu>
2018-04-07 09:00:47 -0500

    prepare for next release of data

--- source/rewriteCSV.R
+++ source/rewriteCSV.R
@@ -7,6 +7,8 @@ df %>%
   rename(long = X) %>%
   rename(lat = Y) %>%
   select(BarrierID, Status, LastUpdate, Type, BarrierCk, CheckType,
-         BarrierCk2, CheckType2, Descrip, long, lat) -> df_clean
+         BarrierCk2, CheckType2, Descrip, long, lat) -> allBarriers
 
-write_csv(df_clean, "spatialData_v4/STL_BARRIERS_All_04072018_clean.csv")
+write_csv(allBarriers, "spatialData_v4/STL_BARRIERS_All_04072018_clean.csv")
+
+save(allBarriers, file = "spatialData_v4/allBarriers.rda")
