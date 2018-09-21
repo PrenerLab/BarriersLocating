c20f19f2ee8d2b81ed141ad922717cbac8912220
Christopher Prener <chris.prener@slu.edu>
2018-09-21 16:20:11 -0500

    new release of spatial data

--- source/rewriteCSV.R
+++ source/rewriteCSV.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(readr)
 
-df <- read_csv("spatialData_v4/STL_BARRIERS_All_04072018.csv")
+df <- read_csv("spatialData_v5/STL_BARRIERS_All_09212018.csv")
 
 df %>%
   rename(long = X) %>%
@@ -9,6 +9,6 @@ df %>%
   select(BarrierID, Status, LastUpdate, Type, BarrierCk, CheckType,
          BarrierCk2, CheckType2, Descrip, long, lat) -> allBarriers
 
-write_csv(allBarriers, "spatialData_v4/STL_BARRIERS_All_04072018_clean.csv")
+write_csv(allBarriers, "spatialData_v5/STL_BARRIERS_All_09212018_clean.csv")
 
-save(allBarriers, file = "spatialData_v4/allBarriers.rda")
+save(allBarriers, file = "spatialData_v5/allBarriers.rda")
